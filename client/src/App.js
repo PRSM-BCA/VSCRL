f95541372fb18289280c151f03f60d260efc0e7e
Jack Lavallee <jacklavallee217@gmail.com>
2021-11-24 11:56:20 -0500

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PRSM!</h1>
+      <p>This is scss...</p>
+      <p>added</p>
+    </div>
+  );
+}
+
+export default App;
