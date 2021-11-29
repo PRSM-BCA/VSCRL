8bd104615fbe1a1ca0af7dc47b5b9aab7903fb5c
Jack Lavallee <jacklavallee217@gmail.com>
2021-11-29 10:46:41 -0500

    Login added to Landing

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -5,13 +5,21 @@ function Landing() {
   return (
     <div className="Landing">
       <h1>PRSM!</h1>
-      <Link to='/KeyWordAnswer'>KeyWordAnswer</Link>
-      <Link to='/LongAnswer'>LongAnswer</Link>
-      <Link to='/MultipleChoice'>MultipleChoice</Link>
-      <Link to='/RankingAnswer'>RankingAnswer</Link>
-      <Link to='/ScaleAnswer'>ScaleAnswer</Link>
-      <Link to='/SelectImg'>SelectImg</Link>
-      <Link to='/ShortAnswer'>ShortAnswer</Link>
+      <nav>
+        <Link to='/KeyWordAnswer'>KeyWordAnswer</Link>
+        <Link to='/LongAnswer'>LongAnswer</Link>
+        <Link to='/MultipleChoice'>MultipleChoice</Link>
+        <Link to='/RankingAnswer'>RankingAnswer</Link>
+        <Link to='/ScaleAnswer'>ScaleAnswer</Link>
+        <Link to='/SelectImg'>SelectImg</Link>
+        <Link to='/ShortAnswer'>ShortAnswer</Link>
+      </nav>
+
+      <form method="GET" action="">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" placeholder="Submit" />
+      </form>
     </div>
   );
 }
