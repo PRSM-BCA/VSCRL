ff247feadae0c89e83df03fe998121229be7cf29
Trentp26 <trentpoitras@gmail.com>
2021-11-29 13:39:04 -0500

    Moving landing signup to signup component

---
+++ client/src/components/SignUp/SignUp.jsx
@@ -0,0 +1,16 @@
+import './SignUp.scss'
+
+
+
+export default function SignUp(){
+    return(
+
+<form method="GET" action="">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" placeholder="Submit" />
+      </form>
+    )
+    }
+
+    
