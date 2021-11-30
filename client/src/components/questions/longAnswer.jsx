d0dec79502cc21a4a8d5810cfe1c019b78509c29
Trentp26 <trentpoitras@gmail.com>
2021-11-30 09:46:34 -0500

    changes to outline of questions

--- client/src/components/questions/longAnswer.jsx
+++ client/src/components/questions/longAnswer.jsx
@@ -1,7 +1,19 @@
 import './Question.scss'
 
 function LongAnswer(props) {
-    return <h1>LongAnswer</h1>
+    return (
+        <section className="longAnswer">
+          <h3>What is typical morning from parking the car to seeing your first patient?</h3>
+          <div className="inputWrapper">
+          <input
+            type="text"
+            name="question answer 2"
+            placeholder="pls enter your question answer"
+          ></input>
+          <p>0/250 max words</p>
+          </div>
+        </section>
+    )
 }
 
 export default LongAnswer
--- client/src/components/questions/shortAnswer.jsx
+++ client/src/components/questions/shortAnswer.jsx
@@ -4,24 +4,33 @@ function ShortAnswer(props) {
   return (
     <div className="ShortAnswer">
       <form>
-        <h3>question 1</h3>
-        <input
-          type="text"
-          name="question answer 1"
-          placeholder="pls enter your question answer"
-        ></input>
-        <h3>question 2</h3>
-        <input
-          type="text"
-          name="question answer 2"
-          placeholder="pls enter your question answer"
-        ></input>
-        <h3>question 3</h3>
-        <input
-          type="text"
-          name="question answer 3"
-          placeholder="pls enter your question answer"
-        ></input>
+        <h3>List 3 frustrations about your footwear</h3>
+        <section>
+          #1
+          <input
+            type="text"
+            name="question answer 1"
+            placeholder="pls enter your question answer"
+          ></input>
+        </section>
+
+        <section>
+          #2
+          <input
+            type="text"
+            name="question answer 2"
+            placeholder="pls enter your question answer"
+          ></input>
+        </section>
+
+        <section>
+          #3
+          <input
+            type="text"
+            name="question answer 3"
+            placeholder="pls enter your question answer"
+          ></input>
+        </section>
       </form>
     </div>
   );
