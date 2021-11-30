1e20088e74de2fec9e50061aa6bef09a42c3cb68
Trentp26 <trentpoitras@gmail.com>
2021-11-30 16:03:43 -0500

    pulling main'

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Landing from './components/landing/Landing';
-
-test('renders learn react link', () => {
-  render(<Landing />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
