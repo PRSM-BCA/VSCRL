6b2151288226e79894786878f6cb8cdb2fae7f57
Sophie <sophiekbodnar@gmail.com>
2021-12-03 13:43:21 -0500

    styling updates to KeyWordAnswer

--- client/src/components/questions/keyWordAnswer.jsx
+++ client/src/components/questions/keyWordAnswer.jsx
@@ -1,7 +1,26 @@
-import './Question.scss'
+import "./Question.scss";
 
 function KeyWordAnswer(props) {
-    return <h1>KeyWordAnswer</h1>
+  return (
+    <>
+      <div className="KeyWordAnswer">
+        <h1 className="TitleHeader">
+          Click 5 frustrations about your footwear
+        </h1>
+        <div className="WordCloud">
+          <div id="painOne">Lack of Cushion</div>
+          <div id="painTwo">Sweaty Feet</div>
+          <div id="painThree"></div>
+          <div id="painFour">Body fluids on footwear</div>
+          <div id="painFive"></div>
+          <div id="painSix">Lack of support</div>
+          <div id="painSeven"></div>
+          <div id="painEight">Hot feet</div>
+          <div id="painNine"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default KeyWordAnswer
+export default KeyWordAnswer;
