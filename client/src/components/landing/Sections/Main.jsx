de35a692df9944941a63c63e7bd369ceea73297d
Sophie <sophiekbodnar@gmail.com>
2021-12-10 16:30:07 -0500

    cleaning up some of the styles that didn't make it in last pull

--- client/src/components/landing/Sections/Main.jsx
+++ client/src/components/landing/Sections/Main.jsx
@@ -4,7 +4,7 @@ import VideoBackground from "../media/pexels-tima-miroshnichenko-5916543.mp4";
 import adventure from "../media/adventure.png";
 import brainstorm from "../media/brainstorm.png";
 import discount from "../media/discount.png";
-import "../../modal/ModalStyles.scss"
+import "../../modal/ModalStyles.scss";
 
 function Login() {
   return (
@@ -72,41 +72,6 @@ function Login() {
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Iaculis nunc sed augue lacus.
             </p>
-          </div>
-          <div className="aboutFlexContainer">
-            <div className="aboutMiniFlex">
-              <img className="aboutImage" src={adventure} />
-              <p className="description">
-                <i id="miniHeader">Live Your Passion</i>
-                &nbsp;
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Iaculis nunc sed augue lacus.
-              </p>
-            </div>
-            <div className="aboutMiniFlex">
-              <img className="aboutImage" src={brainstorm} />
-              {/* <p className="text">Empathic insights</p> */}
-              <p className="description">
-                <i id="miniHeader">Empathic Insights</i>
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Iaculis nunc sed augue lacus.
-              </p>
-            </div>
-            <div className="aboutMiniFlex">
-              <img className="aboutImage" src={discount} />
-              {/* <p className="text">Brand rewards</p> */}
-              <p className="description">
-                <i id="miniHeader">Brand Rewards</i>
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Iaculis nunc sed augue lacus.
-              </p>
-            </div>
           </div>
         </div>
       </div>
