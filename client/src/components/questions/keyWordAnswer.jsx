50e2ab66cba594cdff3fb99c7c795ef4a3c491b1
Sophie <sophiekbodnar@gmail.com>
2021-12-12 01:36:50 -0500

    styling and functionality for Keyword Answer question

--- client/src/components/questions/keyWordAnswer.jsx
+++ client/src/components/questions/keyWordAnswer.jsx
@@ -5,14 +5,106 @@ import { AuthProvider, useAuth } from "../../contexts/AuthContext";
 function KeyWordAnswer(props) {
   const { currentUser, getUser } = useAuth();
   const [userInfo, setUserInfo] = useState("");
-  const [selectStyle, setSelectStyle] = useState();
-  // could set state variable for each pain (painBackground1 etc. setting initial state to color)
+
+  //  set state variable for each pain (painBackground1 etc. setting initial state to color)
+  const [selectPOne, setSelectPOne] = useState("painOne");
+  const [selectPTwo, setSelectPTwo] = useState("painTwo");
+  const [selectPThree, setSelectPThree] = useState("painThree");
+  const [selectPFour, setSelectPFour] = useState("painFour");
+  const [selectPFive, setSelectPFive] = useState("painFive");
+  const [selectPSix, setSelectPSix] = useState("painSix");
+  const [selectPSeven, setSelectPSeven] = useState("painSeven");
+  const [selectPEight, setSelectPEight] = useState("painEight");
+  const [selectPNine, setSelectPNine] = useState("painNine");
+
   // IF statement to handle when click again to return to initial state color
 
-  function transformStyle() {
-    console.log("you just clicked")
-    setSelectStyle("transformStyle");
-    
+  ////-----------------------------------function to handle onClick
+  function changeStyleOne() {
+    console.log("clicked");
+    // Pain One
+    if (selectPOne === "painOne") {
+      setSelectPOne("transformStyle");
+    } else if (selectPOne === "transformStyle") {
+      setSelectPOne("painOne")
+    }
+  }
+  //-----------------------------------
+  function changeStyleTwo() {
+    // Pain Two
+    if (selectPTwo === "painTwo") {
+      setSelectPTwo("transformStyle");
+    } else if (selectPTwo === "transformStyle") {
+      setSelectPTwo("painTwo")
+    }
+  }
+  //-----------------------------------
+  function changeStyleThree() {
+    // Pain Three
+    if (selectPThree === "painThree") {
+      setSelectPThree("transformStyle");
+    } else if (selectPThree === "transformStyle") {
+      setSelectPThree("painThree")
+    }
+  }
+
+  //-----------------------------------
+  function changeStyleFour() {
+    // Pain Four
+    if (selectPFour === "painFour") {
+      setSelectPFour("transformStyle");
+    } else if (selectPFour === "transformStyle") {
+      setSelectPFour("painFour")
+    }
+  }
+
+  //-----------------------------------
+  function changeStyleFive() {
+    // Pain Five
+    if (selectPFive === "painFive") {
+      setSelectPFive("transformStyle");
+    } else if (selectPFive === "transformStyle") {
+      setSelectPFive("painFive")
+    }
+  }
+
+  //-----------------------------------
+  function changeStyleSix() {
+    // Pain six
+    if (selectPSix === "painSix") {
+      setSelectPSix("transformStyle");
+    } else if (selectPSix === "transformStyle") {
+      setSelectPSix("painSix")
+    }
+  }
+
+  //-----------------------------------
+  function changeStyleSeven() {
+    // Pain Seven
+    if (selectPSeven === "painSeven") {
+      setSelectPSeven("transformStyle");
+    } else if (selectPSeven === "transformStyle") {
+      setSelectPSeven("painSeven")
+    }
+  }
+  //-----------------------------------
+  function changeStyleEight() {
+    // Pain Eight
+    if (selectPEight === "painEight") {
+      setSelectPEight("transformStyle");
+    } else if (selectPEight === "transformStyle") {
+      setSelectPEight("painEight")
+    }
+  }
+
+  //-----------------------------------
+  function changeStyleNine() {
+    // Pain Nine
+    if (selectPNine === "painNine") {
+      setSelectPNine("transformStyle");
+    } else if (selectPNine === "transformStyle") {
+      setSelectPNine("painNine")
+    }
   }
 
   useEffect(() => {
@@ -26,42 +118,56 @@ function KeyWordAnswer(props) {
   } else {
     return (
       <>
-          <div className="KeyWordAnswer">
-            <h1 className="TitleHeader">
-              Click 5 frustrations about your footwear
-            </h1>
-           
-              <div className="WordCloud">
-                <div id="painOne" className={selectStyle} onClick={transformStyle}>
-                  Lack of Cushion
-                </div>
-                <div id="painTwo" className={selectStyle} onClick={transformStyle}>
-                  Sweaty Feet
-                </div>
-                <div id="painThree" className={selectStyle} onClick={transformStyle}>
-                  Test
-                </div>
-                <div id="painFour" className={selectStyle} onClick={transformStyle}>
-                  Body fluids on footwear
-                </div>
-                <div id="painFive" className={selectStyle}  onClick={transformStyle}>
-                  Too expensive
-                </div>
-                <div id="painSix" className={selectStyle} onClick={transformStyle}>
-                  Lack of support
-                </div>
-                <div id="painSeven" onClick={transformStyle}>
-                  Never have my size
-                </div>
-                <div id="painEight" onClick={transformStyle}>
-                  Hot feet
-                </div>
-                <div id="painNine" onClick={transformStyle}>
-                  Lack of arch support
-                </div>
-              </div>
-         
+        <div className="KeyWordAnswer">
+          <h1>Click 5 frustrations about your footwear</h1>
 
+          <div className="gridWrapper">
+            <div className="painOne">
+              <div className={selectPOne} onClick={changeStyleOne}>
+                <p>Lack of Cushion</p>
+              </div>
+            </div>
+            <div className="painTwo">
+              <div className={selectPTwo} onClick={changeStyleTwo}>
+                <p>Sweaty Feet</p>
+              </div>
+            </div>
+            <div className="painThree">
+              <div className={selectPThree} onClick={changeStyleThree}>
+                <p>Test</p>
+              </div>
+            </div>
+            <div className="painFour">
+              <div className={selectPFour} onClick={changeStyleFour}>
+                <p>Body fluids on footwear</p>
+              </div>
+            </div>
+            <div className="painFive">
+              <div className={selectPFive} onClick={changeStyleFive}>
+                <p>Too expensive</p>
+              </div>
+            </div>
+            <div className="painSix">
+              <div className={selectPSix} onClick={changeStyleSix}>
+                <p>Lack of support</p>
+              </div>
+            </div>
+            <div className="painSeven">
+              <div className={selectPSeven} onClick={changeStyleSeven}>
+                <p>Never have my half-size</p>
+              </div>
+            </div>
+            <div className="painEight">
+              <div className={selectPEight} onClick={changeStyleEight}>
+                <p>Hot feet</p>
+              </div>
+            </div>
+            <div className="painNine">
+              <div className={selectPNine} onClick={changeStyleNine}>
+                <p>Lack of arch support</p>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
