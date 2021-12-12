6c3f8ee6c2df5f6549934c00fb8dcacc8b30a725
Sophie <sophiekbodnar@gmail.com>
2021-12-12 17:06:47 -0500

    keyword functionality for admin and keyUser

--- client/src/components/questions/keyWordAnswer.jsx
+++ client/src/components/questions/keyWordAnswer.jsx
@@ -16,95 +16,35 @@ function KeyWordAnswer(props) {
   const [selectPSeven, setSelectPSeven] = useState("painSeven");
   const [selectPEight, setSelectPEight] = useState("painEight");
   const [selectPNine, setSelectPNine] = useState("painNine");
+  const [count, setCount] = useState(0);
 
-  // IF statement to handle when click again to return to initial state color
+  //Admin states: create a set of nine state variables (one for each frustration input). Set onChange
+  const [input1, setInput1] = useState(null);
+  const [input2, setInput2] = useState(null);
+  const [input3, setInput3] = useState(null);
+  const [input4, setInput4] = useState(null);
+  const [input5, setInput5] = useState(null);
+  const [input6, setInput6] = useState(null);
+  const [input7, setInput7] = useState(null);
+  const [input8, setInput8] = useState(null);
+  const [input9, setInput9] = useState(null);
 
-  ////-----------------------------------function to handle onClick
-  function changeStyleOne() {
-    console.log("clicked");
-    // Pain One
-    if (selectPOne === "painOne") {
-      setSelectPOne("transformStyle");
-    } else if (selectPOne === "transformStyle") {
-      setSelectPOne("painOne")
+  // Change style function and keep track of count
+  function changeStyle(
+    classType,
+    selectPain,
+    setSelectPain,
+    counter,
+    setCounter
+  ) {
+    if (selectPain === classType && counter < 5) {
+      setSelectPain("transformStyle");
+      setCounter(counter + 1);
+    } else if (selectPain === "transformStyle") {
+      setSelectPain(classType);
+      setCounter(counter - 1);
     }
-  }
-  //-----------------------------------
-  function changeStyleTwo() {
-    // Pain Two
-    if (selectPTwo === "painTwo") {
-      setSelectPTwo("transformStyle");
-    } else if (selectPTwo === "transformStyle") {
-      setSelectPTwo("painTwo")
-    }
-  }
-  //-----------------------------------
-  function changeStyleThree() {
-    // Pain Three
-    if (selectPThree === "painThree") {
-      setSelectPThree("transformStyle");
-    } else if (selectPThree === "transformStyle") {
-      setSelectPThree("painThree")
-    }
-  }
-
-  //-----------------------------------
-  function changeStyleFour() {
-    // Pain Four
-    if (selectPFour === "painFour") {
-      setSelectPFour("transformStyle");
-    } else if (selectPFour === "transformStyle") {
-      setSelectPFour("painFour")
-    }
-  }
-
-  //-----------------------------------
-  function changeStyleFive() {
-    // Pain Five
-    if (selectPFive === "painFive") {
-      setSelectPFive("transformStyle");
-    } else if (selectPFive === "transformStyle") {
-      setSelectPFive("painFive")
-    }
-  }
-
-  //-----------------------------------
-  function changeStyleSix() {
-    // Pain six
-    if (selectPSix === "painSix") {
-      setSelectPSix("transformStyle");
-    } else if (selectPSix === "transformStyle") {
-      setSelectPSix("painSix")
-    }
-  }
-
-  //-----------------------------------
-  function changeStyleSeven() {
-    // Pain Seven
-    if (selectPSeven === "painSeven") {
-      setSelectPSeven("transformStyle");
-    } else if (selectPSeven === "transformStyle") {
-      setSelectPSeven("painSeven")
-    }
-  }
-  //-----------------------------------
-  function changeStyleEight() {
-    // Pain Eight
-    if (selectPEight === "painEight") {
-      setSelectPEight("transformStyle");
-    } else if (selectPEight === "transformStyle") {
-      setSelectPEight("painEight")
-    }
-  }
-
-  //-----------------------------------
-  function changeStyleNine() {
-    // Pain Nine
-    if (selectPNine === "painNine") {
-      setSelectPNine("transformStyle");
-    } else if (selectPNine === "transformStyle") {
-      setSelectPNine("painNine")
-    }
+    console.log("counter", counter);
   }
 
   useEffect(() => {
@@ -113,63 +53,297 @@ function KeyWordAnswer(props) {
     }
   }, [getUser, userInfo, currentUser]);
 
+  // --------------------------------If Admin
+
   if (userInfo.usertype === "admin") {
-    return;
+    return (
+      <AuthProvider>
+        <div className="KeyWordAnswer">
+          <h1>Click 5 frustrations about your footwear (ADMIN)</h1>
+
+          <div className="gridWrapper">
+            <input
+              className="pain painOne "
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput1(evt.target.value);
+              }}
+            ></input>
+            <input
+              className="pain painTwo"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput2(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painThree"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput3(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painFour"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput4(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painFive"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput5(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painSix"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput6(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painSeven"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput7(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painEight"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput8(evt.target.value);
+              }}
+            ></input>
+
+            <input
+              className="pain painNine"
+              type="text"
+              name="keyword"
+              placeholder="Enter"
+              onChange={(evt) => {
+                setInput9(evt.target.value);
+              }}
+            ></input>
+          </div>
+          {input1 &&
+          input2 &&
+          input3 &&
+          input4 &&
+          input5 &&
+          input6 &&
+          input7 &&
+          input8 &&
+          input9 ? (
+            <button className="active" type="submit" onClick={() => {}}>
+              Enter Question Info
+            </button>
+          ) : (
+            <button disabled type="submit" onClick={() => {}}>
+              Enter Question Info
+            </button>
+          )}
+        </div>
+      </AuthProvider>
+    );
+
+    // --------------------------------If Key User
   } else {
     return (
-      <>
+      <AuthProvider>
         <div className="KeyWordAnswer">
+          {console.log(count)}
           <h1>Click 5 frustrations about your footwear</h1>
 
           <div className="gridWrapper">
-            <div className="painOne">
-              <div className={selectPOne} onClick={changeStyleOne}>
-                <p>Lack of Cushion</p>
-              </div>
+            <div
+              className={`pain painOne ${selectPOne}`}
+              onClick={() => {
+                changeStyle(
+                  "painOne",
+                  selectPOne,
+                  setSelectPOne,
+                  count,
+                  setCount
+                );
+                console.log("this is count", count);
+              }}
+            >
+              <p>Lack of Cushion</p>
             </div>
-            <div className="painTwo">
-              <div className={selectPTwo} onClick={changeStyleTwo}>
+            <div className="pain painTwo">
+              <div
+                className={`pain painTwo ${selectPTwo}`}
+                onClick={() => {
+                  changeStyle(
+                    "painTwo",
+                    selectPTwo,
+                    setSelectPTwo,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Sweaty Feet</p>
               </div>
             </div>
-            <div className="painThree">
-              <div className={selectPThree} onClick={changeStyleThree}>
+            <div className="pain painThree">
+              <div
+                className={`pain painThree ${selectPThree}`}
+                onClick={() => {
+                  changeStyle(
+                    "painThree",
+                    selectPThree,
+                    setSelectPThree,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Test</p>
               </div>
             </div>
-            <div className="painFour">
-              <div className={selectPFour} onClick={changeStyleFour}>
+            <div className="pain painFour">
+              <div
+                className={`pain painFour ${selectPFour}`}
+                onClick={() => {
+                  changeStyle(
+                    "painFour",
+                    selectPFour,
+                    setSelectPFour,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Body fluids on footwear</p>
               </div>
             </div>
-            <div className="painFive">
-              <div className={selectPFive} onClick={changeStyleFive}>
+            <div className=" pain painFive">
+              <div
+                className={`pain painFive ${selectPFive}`}
+                onClick={() => {
+                  changeStyle(
+                    "painFive",
+                    selectPFive,
+                    setSelectPFive,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Too expensive</p>
               </div>
             </div>
-            <div className="painSix">
-              <div className={selectPSix} onClick={changeStyleSix}>
+            <div className="pain painSix">
+              <div
+                className={`pain painSix ${selectPSix}`}
+                onClick={() => {
+                  changeStyle(
+                    "painSix",
+                    selectPSix,
+                    setSelectPSix,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Lack of support</p>
               </div>
             </div>
-            <div className="painSeven">
-              <div className={selectPSeven} onClick={changeStyleSeven}>
+            <div className="pain painSeven">
+              <div
+                className={`pain painSeven ${selectPSeven}`}
+                onClick={() => {
+                  changeStyle(
+                    "painSeven",
+                    selectPSeven,
+                    setSelectPSeven,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Never have my half-size</p>
               </div>
             </div>
-            <div className="painEight">
-              <div className={selectPEight} onClick={changeStyleEight}>
+            <div className="pain painEight">
+              <div
+                className={`pain painEight ${selectPEight}`}
+                onClick={() => {
+                  changeStyle(
+                    "painEight",
+                    selectPEight,
+                    setSelectPEight,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Hot feet</p>
               </div>
             </div>
-            <div className="painNine">
-              <div className={selectPNine} onClick={changeStyleNine}>
+            <div className="pain painNine">
+              <div
+                className={`pain painNine ${selectPNine}`}
+                onClick={() => {
+                  changeStyle(
+                    "painNine",
+                    selectPNine,
+                    setSelectPNine,
+                    count,
+                    setCount
+                  );
+                  console.log("this is count", count);
+                }}
+              >
                 <p>Lack of arch support</p>
               </div>
             </div>
           </div>
+          {count === 5 ? (
+            <button className="active" type="submit" onClick={() => {}}>
+              Enter Question Info
+            </button>
+          ) : (
+            <button disabled type="submit" onClick={() => {}}>
+              Enter Question Info
+            </button>
+          )}
         </div>
-      </>
+      </AuthProvider>
     );
   }
 }
